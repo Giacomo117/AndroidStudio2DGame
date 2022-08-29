ec014fe40a41aea1648216fe6c6bdbf985c48634
Giacomo117 <giacky118@gmail.com>
2022-08-29 17:25:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/androidstudio2dgame/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidstudio2dgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/*Main*/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window=getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new Game(this));
+
+
+    }
+}
+
